6bde7d84d3e1248d972f6d60b5e0cbd5f5d781c0
Hardik Desai <hardikd@zignuts.com>
2023-11-01 13:48:36 +0530

    feat: initial Commit

---
+++ frontend/src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Hardik Desai',
+  email: 'hardik@gmail.com',
+  photoURL: 'https://avatars.githubusercontent.com/u/87645745?v=4',
+};
